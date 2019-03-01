665b7ff3ece64d313af1f0efe0c5370c3b2856ce
Eddie Pi <epi@numa-04.uccs.edu>
2019-02-28 17:09:53 -0700

    disable format check

--- workloads/rocksdb/run_rocksdb.sh
+++ workloads/rocksdb/run_rocksdb.sh
@@ -4,12 +4,12 @@ if [ ! -n "$1" ]; then
 else
         operations=10000
         workload=workload-read
-        if [! -n "$2" ]; then
+        if [ ! -n "$2" ]; then
                 operations=10000
         else
                 operations=$2
         fi
-        if [! -n "$3" ]; then
+        if [ ! -n "$3" ]; then
                 workload=workload-read
         else
                 workload=$3
@@ -17,6 +17,6 @@ else
 
         cd /home/epi/YCSB
         DATA_SIZE=$1
-        taskset -c 57-63 bin/ycsb run rocksdb -s -P workloads/$workload -P workloads/$DATA_SIZE -p operationcount=$operations -p rocksdb.dir=/tmp/ycsb-rocksdb-data
+        sudo taskset -c 57-63 bin/ycsb run rocksdb -s -P workloads/$workload -P workloads/$DATA_SIZE -p operationcount=$operations -p rocksdb.dir=/tmp/ycsb-rocksdb-data 
 fi
 
