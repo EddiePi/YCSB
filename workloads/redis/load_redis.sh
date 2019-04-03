4e252c7323071fcafc021abbc3d7667dd3658421
Eddie Pi <epi@numa-03.uccs.edu>
2019-04-03 15:18:08 -0600

    increase redis client timeout

---
+++ workloads/redis/load_redis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -n "$1" ]; then
+        echo "please specify data size"
+else
+        cd /home/epi/YCSB
+        DATA_SIZE=$1
+        bin/ycsb load redis -P workloads/workloada -P workloads/$DATA_SIZE -p "redis.host=numa-04" -p "redis.port=6379"
+fi
+
--- workloads/solr/load_solr.sh
+++ workloads/solr/load_solr.sh
@@ -4,5 +4,5 @@ if [ ! -n "$1" ]; then
 else
         cd /home/epi/YCSB
         DATA_SIZE=$1
-        bin/ycsb load solr -s -P workloads/workloada -P workloads/$DATA_SIZE -p table=ycsb_collection
+        bin/ycsb load solr -s -P workloads/workloada -P workloads/$DATA_SIZE -p table=ycsb -p solr.cloud=true -p solr.zookeeper.hosts=numa-03:2181
 fi
