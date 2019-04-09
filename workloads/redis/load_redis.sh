b08d6ed887b085098c12d472d5f60881a44ebade
EddiePi <piyidi36@sina.come>
2019-04-09 14:10:00 -0600

    increase redis client timeout

--- workloads/redis/load_redis.sh
+++ workloads/redis/load_redis.sh
@@ -4,6 +4,6 @@ if [ ! -n "$1" ]; then
 else
         cd /home/epi/YCSB
         DATA_SIZE=$1
-        bin/ycsb load redis -P workloads/workloada -P workloads/$DATA_SIZE -p "redis.host=numa-04" -p "redis.port=6379"
+        bin/ycsb load redis -s -P workloads/workloada -P workloads/$DATA_SIZE -p "redis.host=numa-04" -p "redis.port=6379"
 fi
 
